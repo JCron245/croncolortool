194ce52108dab5cc742d8b9b637a004cc7616da4
Jon Cornwell <jonnmb@gmail.com>
2020-09-10 23:38:00 -0600

    additional bug fixes

--- craco.config.js
+++ craco.config.js
@@ -6,7 +6,7 @@ module.exports = {
       plugins: [
         purgecss({
 					content: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.ts'],
-					whitelist: ['html', 'head', 'body', 'section', 'div', 'active'],
+					whitelist: ['html', 'head', 'body', 'section', 'div', 'active', 'input-range__slider', 'input-range__label--value', 'input-range__label--min', 'input-range__label--max', 'input-range__track--background', 'input-range__slider-container', 'input-range__track', 'input-range__track--active'],
 					whitelistPatterns: [/^nav/, /^container/, /^input-range/, /^Toastify/, /^custom/, /^chrome/, /^picker/, /^collapse/, /^ml-/, /^mr/, /^fade/, /^animation/, /^slider/],
 					whitelistPatternsChildren: [/^nav/, /^container/, /^input-range/, /^Toastify/, /^custom/, /^chrome/, /^picker/, /^collapse/, /^ml-/, /^mr/, /^fade/, /^animation/, /^slider/]
         }),
