7c0b32bf53d21028d806f5df8024328bf24aa33a
Jon Cornwell <jonnmb@gmail.com>
2020-09-15 15:13:46 -0600

    Fixes

--- craco.config.js
+++ craco.config.js
@@ -5,10 +5,10 @@ module.exports = {
     postcss: {
       plugins: [
         purgecss({
-					content: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.ts'],
-					whitelist: ['html', 'head', 'body', 'section', 'div', 'active', 'input-range__slider', 'input-range__label--value', 'input-range__label--min', 'input-range__label--max', 'input-range__track--background', 'input-range__slider-container', 'input-range__track', 'input-range__track--active'],
-					whitelistPatterns: [/^nav/, /^container/, /^input-range/, /^Toastify/, /^custom/, /^chrome/, /^picker/, /^collapse/, /^ml-/, /^mr/, /^fade/, /^animation/, /^slider/],
-					whitelistPatternsChildren: [/^nav/, /^container/, /^input-range/, /^Toastify/, /^custom/, /^chrome/, /^picker/, /^collapse/, /^ml-/, /^mr/, /^fade/, /^animation/, /^slider/]
+	  content: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.ts'],
+	  whitelist: ['html', 'body', 'section', 'div', 'active', 'input-range__slider', 'input-range__label--value', 'input-range__label--min', 'input-range__label--max', 'input-range__track--background', 'input-range__slider-container', 'input-range__track', 'input-range__track--active'],
+	  whitelistPatterns: [/^nav/, /^container/, /^input-range/, /^Toastify/, /^custom/, /^chrome/, /^picker/, /^collapse/, /^ml-auto/, /^fade/, /^animation/, /^slider/, /slider$/, /label$/, /track$/],
+	  whitelistPatternsChildren: [/^nav/, /^container/, /^input-range/, /^Toastify/, /^custom/, /^chrome/, /^picker/, /^collapse/, /^ml-auto/, /^fade/, /^animation/, /^slider/]
         }),
       ],
     },
